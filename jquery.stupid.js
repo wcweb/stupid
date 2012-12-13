fdaacfceb96e85c35b74054cc014b5e3012ed190
wcrh35@gmail.com <wcrh35@gmail.com>
2012-12-13 17:18:20 +0800

    make it readable

--- jquery.stupid.js
+++ jquery.stupid.js
@@ -29,13 +29,17 @@
   st.defaults = {
     x_axise:[1,2,3,4,5,6,7,8],
     y_axise:[1,2,3,4,5,6,7,8],
-	  blur:undefined,
-    //todo data.row,data.colum
+	blurEvent:undefined,
+    //@todo data.row,data.colum
     data:[],
-	  dtime:[],
+	dtime:[],
     duration:2000,
     size:{'height':20,'width':20,'margin':10,'border':2}
   };
+  var 	_width = st.defaults.size.width,
+		_height = st.defaults.size.height,
+		_border = st.defaults.size.border,
+		_margin = st.defaults.size.margin;
 
 /*
  * input with  x-axise y-axise (Array),gird (double array)
@@ -76,8 +80,13 @@
     st.data=st.dataObject.data;
     st.data[id[1]][id[2]] = value;
   };
+
   // public function
   st.init = function(wrapper,conf){
+	  _width = st.settings.size.width,
+      _height = st.settings.size.height,
+      _border = st.settings.size.border,
+      _margin = st.settings.size.margin;
     if(!unique){
       st.wrapper = wrapper;
       wrapper.addClass('little_boy_family');
@@ -95,13 +104,9 @@
     var rows = dataObject.rows,
         colums = dataObject.colums,
         data = dataObject.data,
-         _width = st.settings.size.width,
-        _height = st.settings.size.height,
-        _border = st.settings.size.border,
-        _margin = st.settings.size.margin;
 		dtime = dataObject.dtime;
-    st.grid = new Array(colums);
-    st.house = $('<div></div>').addClass('little_boy_house');
+    	st.grid = new Array(colums);
+    	st.house = $('<div></div>').addClass('little_boy_house');
     for(var ii=0;ii<colums;ii+=1){
       st.grid[ii]=new Array(rows);
     }
@@ -112,40 +117,31 @@
         // console.log(i+'  '+j);
         // st.grid[i][j].value = data[i][j];
         var inp = document.createElement('input');
-        st.grid[i][j] = $(inp).attr('title', '当前时间点时间：'+ dtime[i][j]+ ' 秒').attr('id','boy-'+i+'-'+j);
-        //st.grid[i][j] = $('<input id="'+'boy-'+i+'-'+j+' " title=" 当前时间点时间：'+ dtime[i][j]+' 秒" />');
+        st.grid[i][j] = $(inp).attr('title', '当前时间点时间：'	+ dtime[i][j]+ ' 秒')
+							  .attr('id','boy-'+i+'-'+j);
         st.grid[i][j].val(data[i][j]);
         st.placeGrids(st.grid[i][j],j,i);
         st.setGridsEvent(st.grid[i][j],j,i);
         st.house.append(st.grid[i][j]);
       }
     }
-    // st.house.offset({top:_height/2+_border,left:_width/2+_border});
     st.house.css('margin-left',_margin+_width/2)
-          .css('margin-top',_margin+_height/2);
-    $(st.house).appendTo($(st.wrapper));
-    var sl=st.house.offset();
+            .css('margin-top',_margin+_height/2)
+    		.appendTo($(st.wrapper));
     placeAxise(rows,colums);
   };
 
   st.placeGrids = function(item,x,y){
-    var _width = st.settings.size.width,
-        _height = st.settings.size.height,
-        _border = st.settings.size.border,
-        _margin = st.settings.size.margin;
-    $(item).addClass('little_boy').width(_width).height(_height)
-    .offset({
-      top:(_height+_border+_margin)*y,
-      left:(_width+_border+_margin)*x
-    });
+
+    $(item).addClass('little_boy')
+		   .width(_width).height(_height)
+    		.offset({
+		      top:(_height+_border+_margin)*y,
+		      left:(_width+_border+_margin)*x
+		    });
   };
   var placeAxise = function(rows,colums){
-
-    var _width = st.settings.size.width,
-        _height = st.settings.size.height,
-        _border = st.settings.size.border,
-        _margin = st.settings.size.margin;
-        
+      
     var axises = $('<div></div>').addClass('LB-axises'),
         xaxise = $('<div></div>').addClass('LB-x-axise'),
         yaxise = $('<div></div>').addClass('LB-y-axise'),
@@ -155,42 +151,26 @@
 
 
     for(var i=0;i<_x.length; i=i+1){
-        var ti = $('<label></label>').html(_x[i])
-                          .width(_width).height(_height);
-        ti.css('margin-left',(_border+_margin))
-            .css('margin-top',0);     
-        // ti.offset({ top:0,
-        //             left:(_width+_border)*i+(_margin)*(i-1)
-        //                       });
-        ti.appendTo(xaxise);
+        $('<label></label>').html(_x[i])
+		  .width(_width).height(_height)
+		  .css('margin-left',(_border+_margin))
+          .css('margin-top',0).appendTo(xaxise);     
     }
-    xaxise.width(rows*(_width+_border+_margin)).height(_height+_border);
+    xaxise.width(rows*(_width+_border+_margin))
+			.height(_height+_border)
+			.css('margin-left',0+_width/2);
 
     for(var j=0;j<_y.length; j+=1){
-        var t=$('<label></label>').html(_y[j])
-              .width(_width).height(_height);
-          // if(!$.browser.mozilla){
-          t.css('margin-top',(_border+_margin))
-            .css('margin-left',0);
-          // t.offset(
-          // {
-          //   top:(_height+_border)*j+(_margin)*(j-1),
-          //   left:0
-          // });
-          t.appendTo(yaxise);
-          // }
+        $('<label></label>').html(_y[j]).width(_width).height(_height)
+			.css('margin-top',(_border+_margin))
+            .css('margin-left',0).appendTo(yaxise);
     }
-    yaxise.height(colums*(_height+_border+_margin)).width(_width+_border);
-
+    yaxise.height(colums*(_height+_border+_margin))
+			.width(_width+_border)
+			.css('margin-top',-(_height+_border)+_height/2);
 
     axises.append(xaxise,yaxise).appendTo(st.wrapper);
-
-    xaxise.css('margin-left',0+_width/2);
-    yaxise.css('margin-top',-(_height+_border)+_height/2);
-
-    var allHeight = colums*(_height+_margin+_border);
-    
-    st.wrapper.height(allHeight)
+    st.wrapper.height(colums*(_height+_margin+_border))
               .width(rows*(_width+_margin+_border));
   };
   st.setGridsEvent = function(item,x,y){
@@ -209,6 +189,7 @@
               keyInValue(that,'t');
               break;
           // case 37:
+		  // @todo case 37
           //left
           // case 38:
             //up
@@ -227,32 +208,27 @@
       }
     });
     var itid;
-    $(item).focusin(function(){
+    $(item).focusin(
+		function(){
       if(st.player){
         itid=window.setInterval(function(){
-           // var time = st.settings.x_axise[x]+ st.settings.y_axise[y]*10;
-
 			var time = st.settings.dtime[y][x];
-            //console.log('seek to : '+time+'  iterval id '+ itid);
             st.player.seek(time);
         },st.settings.duration);
       }
     });
     $(item).focusout(function(){
-      //console.log('clear interval');
       if(itid){
         window.clearInterval(itid);
       }
-
-		if(st.settings.blur!==undefined){
-				st.settings.blur.apply(item,item);			
-		}
+	  if(st.settings.blur!==undefined){
+		st.settings.blur.apply(item,item);			
+	  }
     });
 
   };
   st.output = function(){
-    // if no input data it will be safe to use dataObject
     return st.dataObject.data;
   };
-  //todo st.save      default data => modify data output
+  //@todo st.save      default data => modify data output
 })(jQuery);
